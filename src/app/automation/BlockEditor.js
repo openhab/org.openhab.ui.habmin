1bcc2e519f96a7e648fa3eb0022a52298c5ad3d4
Chris Jackson <chris@cd-jackson.com>
2014-06-18 22:32:04 +0100

    Update blockly to use JSON toolbox definition.

--- src/app/automation/BlockEditor.js
+++ src/app/automation/BlockEditor.js
@@ -26,45 +26,88 @@ define([
                     {
                         path: './blockly/',
                         trashcan: true,
-                        toolbox: '<xml>' +
-                            '<category name="Logic">' +
-                            '<block type="controls_if"></block>' +
-                            '<block type="logic_compare"></block>' +
-                            '<block type="logic_operation"></block>' +
-                            '<block type="logic_negate"></block>' +
-                            '<block type="openhab_iftimer"></block>' +
-                            '<block type="logic_boolean"></block>' +
-                            '</category>' +
-                            '<category name="Math">' +
-                            '<block type="math_number"></block>' +
-                            '<block type="math_arithmetic"></block>' +
-                            '<block type="math_round"></block>' +
-                            '<block type="math_constrain"></block>' +
-                            '</category>' +
-                            '<category name="Items">' +
-                            '<block type="openhab_itemset"></block>' +
-                            '<block type="openhab_itemget"></block>' +
-                            '<block type="openhab_itemcmd"></block>' +
-                            '<block type="openhab_persistence_get"></block>' +
-                            '<block type="variables_set"></block>' +
-                            '<block type="variables_get"></block>' +
-                            '<block type="openhab_constantget"></block>' +
-                            '<block type="openhab_constantset"></block>' +
-                            '<block type="openhab_state_onoff"></block>' +
-                            '<block type="openhab_state_openclosed"></block>' +
-                            '<block type="text"></block>' +
-                            '</category>' +
-                            '</xml>'
-                    });
+                        toolbox: [
+                            {
+                                name: "Logic",
+                                blocks: [
+                                    {type: "controls_if"},
+                                    {type: "logic_compare"},
+                                    {type: "logic_operation"},
+                                    {type: "logic_negate"},
+                                    {type: "openhab_iftimer"},
+                                    {type: "logic_boolean"}
+                                ]
+                            },
+                            {
+                                name: "Math",
+                                blocks: [
+                                    {type: "math_number"},
+                                    {type: "math_arithmetic"},
+                                    {type: "math_round"},
+                                    {type: "math_constrain"}
+                                ]
+                            },
+                            {
+                                name: "Items",
+                                blocks: [
+                                    {type: "openhab_itemset"},
+                                    {type: "openhab_itemget"},
+                                    {type: "openhab_itemcmd"},
+                                    {type: "openhab_persistence_get"},
+                                    {type: "variables_set"},
+                                    {type: "variables_get"},
+                                    {type: "openhab_constantget"},
+                                    {type: "openhab_constantset"},
+                                    {type: "openhab_state_onoff"},
+                                    {type: "openhab_state_openclosed"},
+                                    {type: "text"}
+                                ]
+                            }
+                        ]
 
-                this.initialized = true;
-            },
-            setBlocks: function (blocks) {
-                // Clear any existing workspace
-                if(Blockly.getMainWorkspace() != null)
-                    Blockly.getMainWorkspace().clear();
 
-                Blockly.Json.setWorkspace(Blockly.getMainWorkspace(), blocks);
-            }
-        })
-    });
+                /*'<xml>' +
+                 '<category name="Logic">' +
+                 '<block type="controls_if"></block>' +
+                 '<block type="logic_compare"></block>' +
+                 '<block type="logic_operation"></block>' +
+                 '<block type="logic_negate"></block>' +
+                 '<block type="openhab_iftimer"></block>' +
+                 '<block type="logic_boolean"></block>' +
+                 '</category>' +
+                 '<category name="Math">' +
+                 '<block type="math_number"></block>' +
+                 '<block type="math_arithmetic"></block>' +
+                 '<block type="math_round"></block>' +
+                 '<block type="math_constrain"></block>' +
+                 '</category>' +
+                 '<category name="Items">' +
+                 '<block type="openhab_itemset"></block>' +
+                 '<block type="openhab_itemget"></block>' +
+                 '<block type="openhab_itemcmd"></block>' +
+                 '<block type="openhab_persistence_get"></block>' +
+                 '<block type="variables_set"></block>' +
+                 '<block type="variables_get"></block>' +
+                 '<block type="openhab_constantget"></block>' +
+                 '<block type="openhab_constantset"></block>' +
+                 '<block type="openhab_state_onoff"></block>' +
+                 '<block type="openhab_state_openclosed"></block>' +
+                 '<block type="text"></block>' +
+                 '</category>' +
+                 '</xml>'*/
+            });
+
+        this.initialized = true;
+    },
+    setBlocks
+:
+function (blocks) {
+    // Clear any existing workspace
+    if (Blockly.getMainWorkspace() != null)
+        Blockly.getMainWorkspace().clear();
+
+    Blockly.Json.setWorkspace(Blockly.getMainWorkspace(), blocks);
+}
+})
+})
+;
