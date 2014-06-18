253dae26c45ee2da1388ecdf6e94c78e16770bcf
Chris Jackson <chris@cd-jackson.com>
2014-06-18 15:54:00 +0100

    Update to use Json calls instead of convert XML<>JSON

--- src/app/automation/BlockEditor.js
+++ src/app/automation/BlockEditor.js
@@ -3,12 +3,10 @@ define([
         "dojo/_base/lang",
         "dojo/_base/array",
         "dojo/dom-class",
-        "dijit/layout/ContentPane",
-        "dojox/xml/parser",
-        "dojox/xml/DomParser"//,
+        "dijit/layout/ContentPane"//,
 //        "dblockly/Blockly"
     ],
-    function (declare, lang, array, domClass, Container, xmlParser, DomParser) {
+    function (declare, lang, array, domClass, Container) {
 
         return declare(Container, {
             initialized: false,
@@ -30,7 +28,6 @@ define([
                         trashcan: true,
                         toolbox: '<xml>' +
                             '<category name="Logic">' +
-                            '<block type="openhab_rule"></block>' +
                             '<block type="controls_if"></block>' +
                             '<block type="logic_compare"></block>' +
                             '<block type="logic_operation"></block>' +
@@ -67,11 +64,7 @@ define([
                 if(Blockly.getMainWorkspace() != null)
                     Blockly.getMainWorkspace().clear();
 
-                xmlParser
-                xmlParser.
-
-//                Blockly.Json.setWorkspace(Blockly.getMainWorkspace(), blocks);
-                Blockly.getMainWorkspace().render();
+                Blockly.Json.setWorkspace(Blockly.getMainWorkspace(), blocks);
             }
         })
     });
