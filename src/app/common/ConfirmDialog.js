939cb897ec512aee52e06a8b095c6bd23371fa09
Chris Jackson <chris@cd-jackson.com>
2014-07-10 19:51:38 +0100

    Update confirmation dialog

--- src/app/common/ConfirmDialog.js
+++ src/app/common/ConfirmDialog.js
@@ -1,94 +1,88 @@
 define([
-    'dojo/_base/lang',
-    'dojo/_base/declare',
-    'dojo/_base/Deferred',
-    'dojo/dom-construct',
-    'dijit/Dialog',
-    'dijit/form/Button',
-    "dojo/i18n!dijit/nls/common"
-], function (lang, declare, Deferred, domConstruct, Dialog, Button, langCommon) {
+        "dojo/_base/declare",
+        "dojo/_base/lang",
+        "dojo/on",
+        "dojo/dom",
+        "dojo/Evented",
+        "dojo/_base/Deferred",
+        "dojo/dom-construct",
 
-    /**
-     * @class
-     * @name rfe.DialogConfirm
-     * @extends {dijit.Dialog}
-     * @property {dijit.form.Button} okButton reference to OK button
-     * @property {dijit.form.Button} cancelButton reference to Cancel button
-     * @property {dijit.form.CheckBox} skipCheckBox reference to skipping check box
-     * @property {boolean} hasOkButton create an OK button?
-     * @property {boolean} hasCancelButton create a cancel button
-     * @property {boolean} hasSkipCheckBox create the skipping check box
-     * @property {boolean} hasUnderlay create the dialog underlay?
-     * @property {dojo.Deferred} dfd Deferred
-     * @property {HTMLDivElement} buttonNode reference to div containing buttons
-     */
-    return declare(Dialog, /* @lends rfe.DialogConfirm.prototype */ {
-        okButton: null,
-        cancelButton: null,
-        skipCheckBox: null,
-        hasOkButton: true,
-        hasCancelButton: true,
-        hasUnderlay: true,
-        dfd: null,
-        buttonNode: null,
+        "dijit/_Widget",
+        "dijit/_TemplatedMixin",
+        "dijit/_WidgetsInTemplateMixin",
 
-        /**
-         * Instantiates the confirm dialog.
-         * @constructor
-         * @param {object} props
-         */
-        constructor: function (props) {
-            lang.mixin(this, props);
-        },
+        "dijit/Dialog",
+        "dijit/form/Button",
 
-        /**
-         * Creates the OK/Cancel buttons.
-         */
-        postCreate: function () {
-            this.inherited('postCreate', arguments);
+        "dojo/i18n!dijit/nls/common"
 
-            var label, div, remember = false;
+//        "dijit/Tooltip"
+    ],
+    function (declare, lang, on, dom, Evented, Deferred, domConstruct, _Widget, _TemplatedMixin, _WidgetsInTemplateMixin, Dialog, Button, langCommon) {
 
-            div = domConstruct.create('div', {
-                className: 'dijitDialogPaneContent dialogConfirm'
-            }, this.domNode, 'last');
+        return declare([Dialog, Evented], {
+            title: "Login Dialog",
+            message: "",
 
-            if (this.hasOkButton) {
-                this.okButton = new Button({
-                    label: langCommon.buttonOk,
-                    iconClass: 'habminButtonIcon habminIconOk',
-                    onClick: lang.hitch(this, function () {
-                        this.hide();
-                        this.dfd.resolve(remember);
-                    })
-                }, domConstruct.create('div'));
-                div.appendChild(this.okButton.domNode);
+            attributeMap: lang.delegate(dijit._Widget.prototype.attributeMap, {
+                message: {
+                    node: "messageNode",
+                    type: "innerHTML"
+                }
+            }),
+            constructor: function (/*Object*/ kwArgs) {
+                lang.mixin(this, kwArgs);
+
+                var dialogTemplate = dojo.cache('app.common', 'ConfirmDialog.html');
+                var template = lang.replace(dialogTemplate, {
+                    message: kwArgs.text
+                });
+
+                var contentWidget = new (declare(
+                    [_Widget, _TemplatedMixin, _WidgetsInTemplateMixin],
+                    {
+                        templateString: template
+                    }
+                ));
+                contentWidget.startup();
+                var content = this.content = contentWidget;
+
+                // shortcuts
+                this.submitButton = content.submitButton;
+                this.cancelButton = content.cancelButton;
+            },
+
+            postCreate: function () {
+                this.inherited(arguments);
+
+                // Set the button names - for internationalisation
+                this.submitButton.set("label", langCommon.buttonOk);
+                this.cancelButton.set("label", langCommon.buttonCancel);
+
+                this.connect(this.submitButton, "onClick", lang.hitch(this, this.onSubmit));
+                this.connect(this.cancelButton, "onClick", lang.hitch(this, this.onCancel));
+            },
+            onSubmit: function() {
+                this.destroyRecursive();
+                this.dfd.resolve();
+            },
+            onCancel: function() {
+                this.destroyRecursive();
+                this.dfd.cancel();
+            },
+            /**
+             * Shows the dialog.
+             * @return {Deferred}
+             */
+            show: function () {
+                this.inherited('show', arguments);
+
+                if (!this.hasUnderlay) {
+                    domConstruct.destroy(this.id + '_underlay');	// remove underlay
+                }
+                this.dfd = new Deferred();
+                return this.dfd;
             }
-            if (this.hasCancelButton) {
-                this.cancelButton = new Button({
-                    label: langCommon.buttonCancel,
-                    iconClass: 'habminButtonIcon habminIconCancel',
-                    onClick: lang.hitch(this, function () {
-                        this.hide();
-                        this.dfd.cancel(remember);
-                    })
-                }, domConstruct.create('div'));
-                div.appendChild(this.cancelButton.domNode);
-            }
-            this.buttonNode = div;
-        },
-
-        /**
-         * Shows the dialog.
-         * @return {Deferred}
-         */
-        show: function () {
-            this.inherited('show', arguments);
-            if (!this.hasUnderlay) {
-                domConstruct.destroy(this.id + '_underlay');	// remove underlay
-            }
-            this.dfd = new Deferred();
-            return this.dfd;
-        }
+        })
     });
-});
+
