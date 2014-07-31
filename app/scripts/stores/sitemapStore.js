dde9b960f11061b4aa2b585c652b08e258d3657a
Chris Jackson <chris@cd-jackson.com>
2014-07-31 23:02:28 +0100

    Add slate theme

--- app/scripts/stores/sitemapStore.js
+++
@@ -1,8 +0,0 @@
-HABmin.ApplicationAdapter = DS.RESTAdapter.extend({
-    headers: function() {
-        return {
-            "API_KEY": Ember.get(document.cookie.match(/apiKey\=([^;]*)/), "1"),
-            "ANOTHER_HEADER": "Some header value"
-        };
-    }.property().volatile()
-});
