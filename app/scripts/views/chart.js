a17ff4527daeb2e43e926f92f71369302ab9f330
Chris Jackson <chris@cd-jackson.com>
2014-08-02 10:38:04 +0100

    Working version of resizable item list on chart page

--- app/scripts/views/chart.js
+++ app/scripts/views/chart.js
@@ -1,21 +1,16 @@
-/*
 HABmin.ChartView = Ember.View.extend({
-    init: function () {
-        var view = this;
-
+    didInsertElement: function () {
         var resizeHandler = function () {
-            view.rerender();
+            $("#itemList").height($(window).height() - 210);
         };
 
         this.set('resizeHandler', resizeHandler);
         $(window).bind('resize', this.get('resizeHandler'));
+
+        resizeHandler();
     },
 
     willDestroy: function () {
         $(window).unbind('resize', this.get('resizeHandler'));
     }
 });
-
-
-
-*/
