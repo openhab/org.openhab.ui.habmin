dcb42d8c2612d6fabfd8cad73b650ba365c80639
Chris Jackson <chris@cd-jackson.com>
2014-08-04 10:13:31 +0100

    Add login form

---
+++ src/app/login/login.js
@@ -0,0 +1,42 @@
+angular.module('HABmin.login', ['ngAnimate'])
+
+    .controller('FormCtrl', ['$scope', function ($scope) {
+        // hide error messages until 'submit' event
+        $scope.submitted = false;
+        // hide success message
+        $scope.showMessage = false;
+        // method called from shakeThat directive
+        $scope.submit = function () {
+            // show success message
+            $scope.showMessage = true;
+        };
+    }])
+
+    .directive('shakeThat', ['$animate', function ($animate) {
+        return {
+            require: '^form',
+            scope: {
+                submit: '&',
+                submitted: '='
+            },
+            link: function (scope, element, attrs, form) {
+                // listen on submit event
+                element.on('submit', function () {
+                    // tell angular to update scope
+                    scope.$apply(function () {
+                        // everything ok -> call submit fn from controller
+                        if (form.$valid) {
+                            return scope.submit();
+                        }
+
+                        // show error messages on submit
+                        scope.submitted = true;
+                        // shake that form
+                        $animate.addClass(element, 'shake', function () {
+                            $animate.removeClass(element, 'shake');
+                        });
+                    });
+                });
+            }
+        };
+    }]);
