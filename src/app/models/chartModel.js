03b1994765df4991e0de13c9901d6693c7bcae04
Chris Jackson <chris@cd-jackson.com>
2014-08-25 09:54:19 +0100

    Implement chart cache so that the GUI gets updated if the chart is updated.

--- src/app/models/chartModel.js
+++ src/app/models/chartModel.js
@@ -14,6 +14,7 @@ angular.module('HABmin.chartModel', [
 
     .service('ChartListModel', function ($http, $q) {
         this.url = '/services/habmin/persistence/charts';
+        this.chartList = [];
         this.getList = function () {
             var tStart = new Date().getTime();
             var deferred = $q.defer();
@@ -22,9 +23,12 @@ angular.module('HABmin.chartModel', [
                 .success(function (data) {
                     console.log("Fetch completed in", new Date().getTime() - tStart);
 
-                    console.log("Store completed in", new Date().getTime() - tStart);
+                    // Keep a local copy.
+                    // This allows us to update the data later and keeps the GUI in sync.
+                    this.chartList = data.chart;
+                    console.log("Processing completed in", new Date().getTime() - tStart);
 
-                    deferred.resolve(data.chart);
+                    deferred.resolve(this.chartList);
                 })
                 .error(function (data, status) {
                     deferred.reject(data);
@@ -60,6 +64,13 @@ angular.module('HABmin.chartModel', [
                 $http.put(this.url + "/" + chart.id, chart)
                     .success(function (data) {
                         console.log("PUT completed in", new Date().getTime() - tStart);
+
+                        // Update the name in the cache. This will update the GUI if needed.
+                        angular.forEach(this.chartList, function(c) {
+                            if(c.id === chart.id) {
+                                c.name = chart.name;
+                            }
+                        });
 
                         deferred.resolve(data);
                     })
