81133bf43c1a2219f204489027cd1517ea40f438
Chris Jackson <chris@cd-jackson.com>
2015-01-13 20:17:04 +0000

    fix(UI): Fix sitemap setpoint widget buttons

--- src/app/sitemap/sitemapSetpointWidget.js
+++ src/app/sitemap/sitemapSetpointWidget.js
@@ -18,9 +18,9 @@ angular.module('sitemapSetpointWidget', [
                 '<div class="sitemap-widget-content">' +
                 '  <span ng-style="labelColor">{{widget.label}}</span>' +
                 '  <span class="pull-right">' +
-                '  <button type="button" class="btn btn-primary btn-sm" on-click="click(1)"><span class="fa fa-chevron-circle-up"></span></button>' +
+                '  <button type="button" class="btn btn-primary btn-sm" ng-click="click(1)"><span class="fa fa-chevron-circle-up"></span></button>' +
                 '  <span ng-style="valueColor">{{widget.value}}</span>' +
-                '  <button type="button" class="btn btn-primary btn-sm" on-click="click(-1)"><span class="fa fa-chevron-circle-down"></span></button>' +
+                '  <button type="button" class="btn btn-primary btn-sm" ng-click="click(-1)"><span class="fa fa-chevron-circle-down"></span></button>' +
                 '</div>',
             scope: {
                 itemModel: "=",
@@ -31,21 +31,29 @@ angular.module('sitemapSetpointWidget', [
                     return;
                 }
 
+                // Default the step to 1
+                if($scope.widget.step === undefined) {
+                    $scope.widget.step = 1;
+                }
+
+                // minValue maxValue step
                 $scope.$on('habminGUIRefresh', function (newValue, oldValue) {
                     updateWidget();
                     $scope.$apply();
                 });
-// minValue, maxValue, step
+
                 if ($scope.widget.item !== undefined) {
                     $scope.click = function(val) {
                         console.log("Changed setpoint", $scope.widget.label, val);
-                        if (newValue != $scope.currentValue) {
-                            // Keep a record of the current value so we can detect changes from the GUI
-                            // and avoid changes coming from the server!
-                            $scope.currentValue = newValue;
-                            $scope.$emit('habminGUIUpdate', $scope.widget.item.name,
-                                $scope.currentValue === true ? "ON" : "OFF");
+
+                        $scope.currentValue += ($scope.widget.step * val);
+                        if($scope.widget.minValue !== undefined && $scope.currentValue < parseInt($scope.widget.minValue, 10)) {
+                            $scope.currentValue = parseInt($scope.widget.minValue, 10);
                         }
+                        if($scope.widget.maxValue !== undefined && $scope.currentValue > parseInt($scope.widget.maxValue, 10)) {
+                            $scope.currentValue = parseInt($scope.widget.maxValue, 10);
+                        }
+                        $scope.$emit('habminGUIUpdate', $scope.widget.item.name, $scope.currentValue);
                     };
                 }
 
@@ -53,25 +61,7 @@ angular.module('sitemapSetpointWidget', [
 
                 function updateWidget() {
                     if ($scope.widget.item !== undefined) {
-                        // Handle state translation
-                        switch ($scope.widget.item.type) {
-                            case "DimmerItem":
-                                if (parseInt($scope.widget.item.state, 10) > 0) {
-                                    $scope.value = true;
-                                }
-                                else {
-                                    $scope.value = false;
-                                }
-                                break;
-                            case "SwitchItem":
-                                if ($scope.widget.item.state == "ON") {
-                                    $scope.value = true;
-                                }
-                                else {
-                                    $scope.value = false;
-                                }
-                                break;
-                        }
+                        $scope.value = $scope.widget.item.state;
                     }
 
                     if ($scope.widget.labelcolor != null) {
@@ -83,7 +73,7 @@ angular.module('sitemapSetpointWidget', [
 
                     // Keep a record of the current value so we can detect changes from the GUI
                     // and avoid changes coming from the server!
-                    $scope.currentValue = $scope.value;
+                    $scope.currentValue = parseInt($scope.value, 10);
                 }
             }
         };
