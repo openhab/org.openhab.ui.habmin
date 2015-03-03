df577beef2b75942d98bac1e5fd16984a8d9c797
Chris Jackson <chris@cd-jackson.com>
2015-03-03 22:29:41 +0000

    Update item model

--- src/app/models/itemModel.js
+++ src/app/models/itemModel.js
@@ -12,13 +12,15 @@ angular.module('HABmin.itemModel', [
 ])
 
     .service('ItemModel', function ($http, $q, UserService) {
-        this.url = UserService.getServer() + '/rest/items';
+        var svcName = "items";
+        var itemList = [];
+        var url = UserService.getServer() + '/rest/items';
 
-        this.socket = null;
+        var socket = null;
         this.sendCommand = function (item, value) {
             console.log("Sending command", item, value);
             var deferred = $q.defer();
-            $http.post(this.url + "/" + item, value, {
+            $http.post(url + "/" + item, value, {
                     headers: {'Content-Type': 'text/plain'}
                 }
             ).success(function (data, status) {
@@ -28,6 +30,34 @@ angular.module('HABmin.itemModel', [
                     deferred.reject(data);
                 });
 
+            return deferred.promise;
+        };
+
+        this.getList = function () {
+            var tStart = new Date().getTime();
+            var deferred = $q.defer();
+
+            RestService.getService(svcName).then(
+                function (url) {
+                    $http.get(url)
+                        .success(function (data) {
+                            console.log("Fetch completed in", new Date().getTime() - tStart);
+
+                            // Keep a local copy.
+                            // This allows us to update the data later and keeps the GUI in sync.
+                            itemList = [].concat(data);
+                            console.log("Processing completed in", new Date().getTime() - tStart);
+
+                            deferred.resolve(itemList);
+                        })
+                        .error(function (data, status) {
+                            deferred.reject(data);
+                        });
+                },
+                function () {
+                    deferred.reject(null);
+                }
+            );
             return deferred.promise;
         };
     });
