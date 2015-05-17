1571f0b7e7e1cb3fa28fce3e40f58a0610050972
Chris Jackson <chris@cd-jackson.com>
2015-05-17 22:41:11 +0100

    feat(UI): Add support for new OpenHAB dashboard service

---
+++ openhab2/org.openhab.ui.habmin/src/main/java/org/openhab/ui/habmin/internal/HABminUIDashboardTile.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.ui.habmin.internal;
+
+import org.openhab.ui.dashboard.DashboardTile;
+
+/**
+ * The dashboard tile for the HABmin UI
+ * 
+ * @author Chris Jackson
+ *
+ */
+public class HABminUIDashboardTile implements DashboardTile {
+
+    @Override
+    public String getName() {
+        return "HABmin";
+    }
+
+    @Override
+    public String getUrl() {
+        return "../habmin/index.html";
+    }
+
+    @Override
+    public String getOverlay() {
+        return "html5";
+    }
+}
