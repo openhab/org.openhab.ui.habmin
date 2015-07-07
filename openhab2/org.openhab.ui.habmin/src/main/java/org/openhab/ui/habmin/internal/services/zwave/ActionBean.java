c9e8f2bee79e9aaf25f6b3ccf7702dad05476baf
Chris Jackson <chris@cd-jackson.com>
2015-07-07 18:42:02 +0200

    Update zwave

--- openhab2/org.openhab.ui.habmin/src/main/java/org/openhab/ui/habmin/internal/services/zwave/ActionBean.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.ui.habmin.internal.services.zwave;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name="action")
-public class ActionBean {
-	public String domain;
-	public String action;
-}
