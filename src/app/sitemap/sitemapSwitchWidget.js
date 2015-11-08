5028e13e905a8c1cbdbde9ebfac77eb85cadc43c
Chris Jackson <chris@cd-jackson.com>
2015-11-08 12:26:02 +0000

    feat(Sitemap): Add switch mappings. Closes #92

--- src/app/sitemap/sitemapSwitchWidget.js
+++ src/app/sitemap/sitemapSwitchWidget.js
@@ -20,42 +20,51 @@ angular.module('sitemapSwitchWidget', [
             '  <span class="pull-right">' +
             '  <span ng-style="valueColor"></span>' +
             '  <small>' +
-            '    <toggle-switch ng-if="toggle" ng-model="value2" on-label="ON" off-label="OFF"></toggle-switch>' +
+            '    <toggle-switch ng-if="toggle" ng-model="value" on-label="ON" off-label="OFF"></toggle-switch>' +
             '    <ui-select ng-if="!toggle"' +
-            '        ng-model="value1"' +
-            '        theme="bootstrap"'+
+            '        ng-model="value"' +
+            '        theme="bootstrap"' +
+            '        on-select="selectUpdate($item)"' +
             '        search-enabled="false">' +
-            '      <ui-select-match>{{$select.selected.label}}'+
-            '      </ui-select-match>'+
-            '      <ui-select-choices repeat="mapping.command as mapping in widget.mappings">'+
-            '        <div>{{mapping.label}}</div>'+
-            '      </ui-select-choices>'+
+            '      <ui-select-match>{{$select.selected.label}}' +
+            '      </ui-select-match>' +
+            '      <ui-select-choices repeat="mapping.command as mapping in widget.mappings">' +
+            '        <div>{{mapping.label}}</div>' +
+            '      </ui-select-choices>' +
             '    </ui-select>' +
             '  </small>' +
             '</div>',
             scope: {
                 itemId: "@",
                 itemModel: "=",
-                widget: "=",
-                value: "="
+                widget: "="
             },
             link: function ($scope, element, attrs, controller) {
                 if ($scope.widget === undefined) {
                     return;
                 }
 
-                if($scope.widget.mappings != null && $scope.widget.mappings.length > 0) {
+                $scope.selectUpdate = function (item) {
+                    if (item.command != $scope.currentValue) {
+                        // Keep a record of the current value so we can detect changes from the GUI
+                        // and avoid changes coming from the server!
+                        $scope.currentValue = item.command;
+                        $scope.$emit('habminGUIUpdate', $scope.widget.item.name, $scope.currentValue);
+                    }
+                };
+
+                if ($scope.widget.mappings != null && $scope.widget.mappings.length > 0) {
                     $scope.toggle = false;
 
                     if ($scope.widget.item !== undefined) {
-                        $scope.$watch('value1', function (newValue, oldValue) {
+                        $scope.$watch('value', function (newValue, oldValue) {
                             console.log("Changed switch", $scope.widget.label, newValue, oldValue);
                             if (newValue != $scope.currentValue) {
                                 // Keep a record of the current value so we can detect changes from the GUI
                                 // and avoid changes coming from the server!
                                 $scope.currentValue = newValue;
-//                                $scope.$emit('habminGUIUpdate', $scope.widget.item.name,
-//                                    $scope.currentValue === true ? "ON" : "OFF");
+                                $scope.$emit('habminGUIUpdate', $scope.widget.item.name,
+                                    $scope.currentValue === true ? "ON" : "OFF");
                             }
                         });
                     }
@@ -64,7 +73,7 @@ angular.module('sitemapSwitchWidget', [
                     $scope.toggle = true;
 
                     if ($scope.widget.item !== undefined) {
-                        $scope.$watch('value2', function (newValue, oldValue) {
+                        $scope.$watch('value', function (newValue, oldValue) {
                             console.log("Changed switch", $scope.widget.label, newValue, oldValue);
                             if (newValue != $scope.currentValue) {
                                 // Keep a record of the current value so we can detect changes from the GUI
@@ -102,6 +111,9 @@ angular.module('sitemapSwitchWidget', [
                                     $scope.value = false;
                                 }
                                 break;
+                            case "NumberItem":
+                                $scope.value = $scope.widget.item.state;
+                                break;
                         }
                     }
 
@@ -119,19 +131,23 @@ angular.module('sitemapSwitchWidget', [
 
                 // And then watch for changes
                 $scope.$on('smarthome/items/' + $scope.itemId + "/state", function (event, state) {
-                    if(state.type == "OnOffType") {
+                    if (state.type == "OnOffType") {
                         $scope.value = (state.value == "ON") ? true : false;
                     }
                     else {
-                        var num = Number(state.value);
-                        if (!isNaN(num)) {
-                            $scope.value = num == 0 ? false : true;
-
-                            // Keep a record of the current value so we can detect changes from the GUI
-                            // and avoid changes coming from the server!
-                            $scope.currentValue = $scope.value;
+                        if ($scope.toggle == true) {
+                            var num = Number(state.value);
+                            if (!isNaN(num)) {
+                                $scope.value = num == 0 ? false : true;
+                            }
+                        } else {
+                            $scope.value = state.value;
                         }
                     }
+                    // Keep a record of the current value so we can detect changes from the GUI
+                    // and avoid changes coming from the server!
+                    $scope.currentValue = $scope.value;
+
 //                    $scope.$apply();
                 });
 
