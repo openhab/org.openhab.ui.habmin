4704ff446f5ca0236e50ee6c64d49e257d451d30
Chris Jackson <chris@cd-jackson.com>
2016-03-04 21:01:08 +0000

    feat(ZWave): Add network diagram for OH2 binding
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 HABmin2
 =======
 
-HABmin version2 - ultimately targeted toward openHAB2.
-
-**BREAKING CHANGE:** I've now added ZWave support for the OpenHAB1 binding and therefore until there is a native
-OpenHAB2 ZWave binding, the OpenHAB1 ZWave binding must be installed for HABmin2 to work under OpenHAB2.
+HABmin version2 - for openHAB2.
 
 An **OpenHAB2** bundle is now available with initial support for charting and some OH2 features such
 as listing the newly discovered devices. I hope to keep a single source base that supports as many
@@ -45,23 +42,8 @@ For openHAB-2, download the _org.openhab.ui.habmin_ JAR file from either the
 [releases folder](https://github.com/cdjackson/HABmin2/releases) or you can use the latest snapshot in the
 the [working folder](https://github.com/cdjackson/HABmin2/tree/master/output) and place it in the ```addons``` folder.
 
-**NOTE**. Currently HABmin2 supports a backward compatible mode for the OpenHAB1 ZWave binding. Therefore
-  you must also install the OpenHAB1 zwave JAR into the ```addons``` folder. As soon as a OpenHAB2 ZWave binding
-  is available, this dependency will be removed.
-
 Then open your browser at http://openhab server/habmin/index.html or follow the link from the OpenHAB dashboard.
 
-### OpenHAB-1
-You need to install the [HABmin JAR](https://github.com/cdjackson/HABmin/blob/master/addons/org.openhab.io.habmin-1.7.0-SNAPSHOT.jar?raw=true) **AND** the [ZWave JAR](https://github.com/cdjackson/HABmin/blob/master/addons/org.openhab.binding.zwave-1.7.0-SNAPSHOT.jar?raw=true) from the [HABmin-1 repository](https://github.com/cdjackson/HABmin) and add them to your openHAB ```addons``` folder  (note that you can use any other recent zwave binding if you prefer). These files are needed no matter what installation you choose next - if you don't install these files, some things may work, but most won't (eg sitemaps might work, but charting etc won't). Note that the zwave binding is needed even if you don't have zwave installed - it won't do anything, but is needed to resolve some dependancies - this will be removed in openHAB2.  You also (currently) need to install HABmin version 1 since the HABmin JAR is using a few files in this repository (I know this is bad - I should remove this dependancy soon).
-
-Packages are available for easy install - either for a web server (ie. installation into openHAB so you can use a browser), or as an Android app.
-
-* You can download either the release version - grab the latest from the [releases folder](https://github.com/cdjackson/HABmin2/releases).
-* Or, if you want the latest snapshot, grab it from the [working folder](https://github.com/cdjackson/HABmin2/tree/master/output).
-
-Then grab the appropriate ZIP file from the [releases folder](https://github.com/cdjackson/HABmin2/releases) or the latest snapshot from the [working folder](https://github.com/cdjackson/HABmin2/tree/master/output). Generally the ```-release``` version, however a ```-debug``` version is also supplied.  Unzip the file into a folder webapps/habmin2 in your openhab installation folder.
-
-Then open the browser at http://openhab server/habmin2.
 
 ### Android
 The release also contains an Android package (```apk``` file) - this can be downloaded to an Android device - no further installation is required in openhab (other than the JAR files mentioned above). When the app starts, it should ask you for server and login credentials. I'm still trying to get the reconnection sorted, so it's possible that you may need to log out, and log on to get the connect back when you start again. Please feel free to open an issue and report your findings.
---
+++ src/app/tools/zwave/zwaveNetwork.js
@@ -0,0 +1,291 @@
+/**
+ * HABmin - Home Automation User and Administration Interface
+ * Designed for openHAB (www.openhab.com)
+ *
+ * This software is copyright of Chris Jackson under the GPL license.
+ * Note that this licence may be changed at a later date.
+ *
+ * (c) 2014-2015 Chris Jackson (chris@cd-jackson.com)
+ */
+angular.module('ZWaveNetwork', [
+    'ui.router',
+    'ui.bootstrap',
+    'ngLocalize',
+    'HABmin.userModel',
+    'HABmin.restModel',
+    'HABmin.thingModel',
+    'angular-growl',
+    'Binding.config',
+    'ngVis',
+    'ResizePanel'
+])
+
+    .config(function config($stateProvider) {
+        $stateProvider.state('tools/zwave/network', {
+            url: '/tools/zwave/network',
+            views: {
+                "main": {
+                    controller: 'ZwaveNetworkCtrl',
+                    templateUrl: 'tools/zwave/zwaveNetwork.tpl.html'
+                }
+            },
+            data: {pageTitle: 'ZWave Network'},
+            resolve: {
+                // Make sure the localisation files are resolved before the controller runs
+                localisations: function (locale) {
+                    return locale.ready('zwave');
+                }
+            }
+        });
+    })
+
+    .controller('ZwaveNetworkCtrl',
+    function ZwaveNetworkCtrl($scope, locale, growl, $timeout, $window, $http, $interval, ThingModel, UserService, RestService) {
+        var deviceClassIcons = {
+            "PC_CONTROLLER": "desktop-computer",
+            "PORTABLE_REMOTE_CONTROLLER": "remote-control",
+            "POWER_SWITCH_BINARY": "switch",
+            "POWER_SWITCH_MULTILEVEL": "light-control",
+            "ROUTING_SENSOR_BINARY": "door-open",
+            "SWITCH_REMOTE_MULTILEVEL": "temperature",
+            "ALARM_SENSOR_ROUTING": "alarm"
+        };
+
+        $scope.devices = [];
+
+        ThingModel.getList().then(
+            function (list) {
+                var bridge;
+
+                // Get all the Z-Wave devices
+                angular.forEach(list, function(thing) {
+                    // Check if there are properties, and if so, if this is a z-wave node
+                    if(thing.properties == null) {
+                        return;
+                    }
+                    if(thing.properties["zwave_nodeid"] == null) {
+                        return;
+                    }
+
+                    // Is this our controller?
+                    if(thing.bridgeUID == null) {
+                        bridge = thing;
+                    }
+
+                    // Add it...
+                    $scope.devices.push(thing);
+                });
+
+                if(bridge != null) {
+                    createNetworkMap(bridge);
+                }
+            }
+        );
+
+        $scope.networkOptions = {
+            layout: {
+                hierarchical: {
+                    enabled: true,
+//                    layout: "direction",
+                    sortMethod: "directed",
+                    direction: "UD"
+                }
+            },
+            width: '100%',
+            height: '100%',
+            edges: {
+                color: '#ffffff',
+                width: 5
+            },
+            interaction: {
+                dragNodes: true
+            }
+        };
+
+        $scope.showPanel = function (panel) {
+            if ($scope.panelDisplayed == panel) {
+                $scope.panelDisplayed = "";
+            }
+            else {
+                $scope.panelDisplayed = panel;
+            }
+        };
+
+        function createNetworkMap(root) {
+            // We use 'doneNodes' to keep track of what nodes we've added
+            var doneNodes = [root.properties["zwave_nodeid"]];
+
+            // First, add all the routes
+            var edges = [];
+            angular.forEach($scope.devices, function (device) {
+                if (device.properties["zwave_neighbours"] !== undefined && doneNodes.indexOf(device.UID) == -1) {
+                    doneNodes.push(device.properties["zwave_nodeid"]);
+                }
+
+                // Add all the neighbour routes
+                var neighbours = [];
+                if(device.properties["zwave_neighbours"] != null) {
+                    neighbours = device.properties["zwave_neighbours"].split(",");
+                }
+                angular.forEach(neighbours, function (neighbour) {
+                    // Check if the route exists and mark it as bidirectional
+                    var found = false;
+                    angular.forEach(edges, function (edge) {
+                        if (edge.from == neighbour && edge.to == device.properties["zwave_nodeid"]) {
+                            edge.color = "green";
+                            edge.style = "line";
+                            edge.width = 3;
+
+                            found = true;
+                        }
+                    });
+
+                    if (found === false) {
+                        var newEdge = {};
+                        newEdge.from = device.properties["zwave_nodeid"];
+                        newEdge.to = neighbour;
+                        newEdge.color = "red";
+                        newEdge.style = "arrow";
+                        newEdge.width = 1;
+                        edges.push(newEdge);
+
+                        // Remember this node!
+                        if (doneNodes.indexOf(neighbour) == -1) {
+                            doneNodes.push(neighbour);
+                        }
+                    }
+                });
+            });
+
+            // Now, add all the nodes that have routes
+            var nodes = [];
+            angular.forEach($scope.devices, function (device) {
+                // Only add nodes that have routes
+                if (doneNodes.indexOf(device.properties["zwave_nodeid"]) == -1) {
+                    return;
+                }
+
+                console.log("Processing", device.properties["zwave_nodeid"]);
+                if (device.properties["zwave_neighbours"] === undefined) {
+                    console.log("No neighbors for node ", device.properties["zwave_nodeid"]);
+                }
+                // Add the node
+                var newNode = {};
+                newNode.id = device.properties["zwave_nodeid"];
+                newNode.label = device.properties["zwave_nodeid"];//device.label;
+                if (root === device.properties["zwave_nodeid"]) {
+                    newNode.level = 0;
+                }
+                else {
+                    newNode.level = -1;
+                }
+
+                newNode.borderWidth = 2;    // TODO: put this in general options?
+                newNode.color = {};
+
+                if (device.listening === false) {
+                    newNode.color.background = "grey";
+                }
+                switch (device.statusInfo.status) {
+                    case "ONLINE":
+                        newNode.color.border = "green";
+                        break;
+                    case "OFFLINE":
+                        newNode.color.border = "red";
+                        break;
+                    default:
+                        newNode.color.border = "grey";
+                        break;
+                }
+
+                nodes.push(newNode);
+            });
+
+            // Add all the neighbors from the root
+//            var rootDevice = $scope.devices[root];
+//            if (rootDevice === undefined) {
+//                return;
+//            }
+
+            // Check the root devices neighbors
+            doneNodes = [root.properties["zwave_nodeid"]];
+            var neighbours = [];
+            if(root.properties["zwave_neighbours"] != null) {
+                neighbours = root.properties["zwave_neighbours"].split(",");
+            }
+            angular.forEach(neighbours, function (neighbour) {
+                setNodeLevel(neighbour, 1);
+            });
+
+            for (var level = 1; level < 5; level++) {
+                checkNodeLevel(level);
+            }
+
+            // Calculate the max level, and also set the root position
+            var maxLevel = 0;
+            angular.forEach(nodes, function (node) {
+                if(node.id == root.properties["zwave_nodeid"]) {
+                    node.level = 0;
+                }
+
+                if (node.level > maxLevel) {
+                    maxLevel = node.level;
+                }
+            });
+
+            // Move all unconnected nodes up to the last level
+            maxLevel += 1;
+            angular.forEach(nodes, function (node) {
+                if (node.level == -1) {
+                    node.level = maxLevel;
+                }
+            });
+
+            console.log("Setting network options");
+            $timeout(function () {
+                $scope.networkNodes = {nodes: nodes, edges: edges};
+            });
+            console.log("Setting network options DONE");
+
+            function setNodeLevel(nodeId, level) {
+                angular.forEach(nodes, function (node) {
+                    if (node.id == nodeId) {
+                        node.level = level;
+                        doneNodes.push(nodeId);
+                    }
+                });
+            }
+
+            function checkNodeLevel(level) {
+                angular.forEach(nodes, function (node) {
+                    if (node.level !== level) {
+                        return;
+                    }
+
+                    // Get this device
+                    var device = $scope.devices[node.id];
+                    if (device === undefined) {
+                        return;
+                    }
+                    // Check this devices neighbours
+                    var neighbours = [];
+                    if(device.properties["zwave_neighbours"] != null) {
+                        neighbours = device.properties["zwave_neighbours"].split(",");
+                    }
+                    angular.forEach(neighbours, function (neighbour) {
+                        // Is this node already set?
+                        if (doneNodes.indexOf(neighbour) != -1) {
+                            return;
+                        }
+
+                        setNodeLevel(neighbour.name, level + 1);
+                    });
+                });
+            }
+        }
+    })
+
+;
+
+
+
