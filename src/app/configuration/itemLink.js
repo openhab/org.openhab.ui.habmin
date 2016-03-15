9288e54a6f025e4b0e65cab6ca2fd683f85080a6
Chris Jackson <chris@cd-jackson.com>
2016-03-15 13:30:11 +0000

    chore(UI): Updated German and Italian translations

---
+++ src/app/configuration/itemLink.js
@@ -0,0 +1,70 @@
+/**
+ * HABmin - Home Automation User and Administration Interface
+ * Designed for openHAB (www.openhab.com)
+ *
+ * This software is copyright of Chris Jackson under the GPL license.
+ * Note that this licence may be changed at a later date.
+ *
+ * (c) 2014-2015 Chris Jackson (chris@cd-jackson.com)
+ */
+angular.module('Config.ItemLink', [
+    'ui.bootstrap',
+    'ngSanitize',
+    'angular-growl',
+    'HABmin.itemModel',
+    'ngLocalize'
+])
+    .service('itemLink',
+    function ($modal, $rootScope, growl, locale, UserService, ItemModel, SmartHomeModel) {
+        this.edit = function (thing, channel, item, create) {
+            var scope = $rootScope.$new();
+            scope.result = {item: ""};
+            ItemModel.getList().then(function (list) {
+                scope.items = list
+            });
+
+            /**
+             * Controller functions get called when the modal closes
+             * @param $scope
+             * @param $modalInstance
+             */
+            var controller = function ($scope, $modalInstance) {
+                $scope.ok = function (result) {
+                    ItemModel.linkItem(thing, channel, scope.result.item).then(
+                        function () {
+                            $modalInstance.close($scope.item);
+                            growl.success(locale.getString("item.SaveOk",
+                                {name: $scope.item.label}));
+                        },
+                        function (response) {
+                            var msg;
+                            if (response != null && response.error != null) {
+                                msg = response.error.message;
+                            }
+                            else {
+                                msg = locale.getString("common.noResponse");
+                            }
+                            growl.warning(locale.getString("item.SaveLinkFailed",
+                                {name: $scope.item.label, message: msg}));
+                        }
+                    );
+                };
+                $scope.cancel = function (result) {
+                    $modalInstance.dismiss('cancel');
+                };
+            };
+
+            return $modal.open({
+                backdrop: 'static',
+                keyboard: true,
+                modalFade: true,
+                size: 'lg',
+                templateUrl: 'configuration/itemLink.tpl.html',
+                controller: controller,
+                windowClass: UserService.getTheme(),
+                scope: scope
+            }).result;
+        };
+    })
+
+;
